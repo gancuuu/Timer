c3fbbf34d3c2a0ea9b6a63f991b122064f26d986
gancuuu <kamilganc2009@gmail.com>
2026-04-21 08:38:27 +0200

    Gra ogl kod

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.onaar.timer"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.onaar.timer"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/onaar/timer/MainActivity.java
+++ app/src/main/java/com/onaar/timer/MainActivity.java
@@ -3,18 +3,25 @@
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.os.Handler;
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textViewTimer;
+    private Button[] buttons;
+    private Random random;
+    private Handler handler;
+    private Runnable runnable;
+    private int currentNumber;
 
-       Button btnNr1, btnNr2, btnNr3, btnNr4, btnNr5;
-       TextView textViewTimer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +32,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnNr1 = findViewById(R.id.btnNr1);
-        btnNr2 = findViewById(R.id.btnNr2);
-        btnNr3 = findViewById(R.id.btnNr3);
-        btnNr4 = findViewById(R.id.btnNr4);
-        btnNr5 = findViewById(R.id.btnNr5);
+        buttons = new Button[] {
+                findViewById(R.id.btnNr1),
+                findViewById(R.id.btnNr2),
+                findViewById(R.id.btnNr3),
+                findViewById(R.id.btnNr4),
+                findViewById(R.id.btnNr5)
+        };
         textViewTimer = findViewById(R.id.textViewTimer);
+
+        random = new Random();
+
+        for (Button button : buttons) {
+            button.setText(String.valueOf(random.nextInt(10) + 1));
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Integer.parseInt(((Button) v).getText().toString()) == currentNumber) {
+                        v.setEnabled(false);
+                        v.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+                    }
+                }
+            });
+        }
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                currentNumber = random.nextInt(10) + 1;
+                textViewTimer.setText(String.valueOf(currentNumber));
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(runnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+
     }
 }
